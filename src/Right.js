80abfedd143bab1e0ecb34eace5593d85d3dd125
boscomou <153170298+boscomou@users.noreply.github.com>
2025-02-27 17:57:43 +0800

    Update Right.js

--- src/Right.js
+++ src/Right.js
@@ -2,19 +2,32 @@ import React from 'react'
 import SearchTab from './SearchTab'
 import ChatTab from './ChatTab'
 import UserTag from './UserTag'
+import { auth, googleProvider } from 'c:/Users/bosco/Downloads/React-ChatApp-main/src/config/firebase'
+import { signOut } from 'firebase/auth'
+import {Link, useNavigate} from "react-router-dom"
 import './Right.css';
 
 
 
 function Right() {
-  
+
+    const navigate = useNavigate();
+
+    const logOut = async () => {
+        try {
+          await signOut(auth)
+          navigate("/")
+        } catch (err) {
+          console.error(err);
+        }
+      }
   
 
   return (
     <div className="rightScreen" >
 
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <button>Logout</button>
+        <button onClick={logOut}>Logout</button>
       </div>
 
 
